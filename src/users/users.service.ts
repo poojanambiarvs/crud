7416801f886acc3a1f07199aeb2d72f103d72954
Pooja Nambiar <nambiarpooja74@gmail.com>
2022-10-25 12:42:03 +0530

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  private users= [{id:1,name:"pooja",place:"kannur"}]
+  create(createUserDto: CreateUserDto) {
+    this.users.push(createUserDto);
+    return 'This action adds a new user';
+    
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
